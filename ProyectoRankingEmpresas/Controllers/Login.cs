a770c34d62831f8a80e3f2615c9b0a561194f7be
Bryan-Taday <steventaday8@hotmail.com>
2020-11-26 00:31:23 -0500

    prueba Elias

--- ProyectoRankingEmpresas/Controllers/Login.cs
+++ ProyectoRankingEmpresas/Controllers/Login.cs
@@ -9,7 +9,7 @@ using ProyectoRankingEmpresas.Jwt;
 using AuthorizeAttribute = ProyectoRankingEmpresas.Jwt.AuthorizeAttribute;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+// :V
 namespace ProyectoRankingEmpresas.Controllers
 {
     [Route("api/[controller]")]
